17695aade9d6f4ebb6c1bf921c4e557e1a36157f
bs <bogdan.sachkovskyi@gmail.com>
2012-09-20 21:46:38 +0300

    add kindle 3 (3.2.1) support

--- KindlHighlightViewer/KindlHighlightViewer/Code/Loaders/TxtDataLoader.cs
+++ KindlHighlightViewer/KindlHighlightViewer/Code/Loaders/TxtDataLoader.cs
@@ -25,18 +25,42 @@ namespace KindlHighlightViewer.Code
             try
             {
                 List<ClippingItem> clippingsList = new List<ClippingItem>();
-                string pattern = @"(?<title>.+ )\((?<author>.+)\)";
+
+                string pattern = String.Empty;
+                string newKindle = @"(?<title>.+ )\((?<author>.+)\)";
+                string oldKindle = @"(?<title>.+ )- (?<author>.+)";
+
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string temp = reader.ReadLine();
+                    if (Regex.IsMatch(temp, oldKindle)) // new kindle file
+                    {
+                        pattern = oldKindle;
+                    }
+                    else
+                    {
+                        if (Regex.IsMatch(temp, newKindle)) // old kindle file (Kindle 3 (3.2.1))
+                        {
+                            pattern = newKindle;
+                        }
+                        else
+                        {
+                            ShowBox.ShowError("Bad file!");
+                        }
+                    }
+                }
+
                 using (StreamReader reader = new StreamReader(path))
                 {
                     while (!reader.EndOfStream)
                     {
                         ClippingItem item = new ClippingItem();
                         string tempTandA = reader.ReadLine(); // title (author) string
-                        item.Title = Regex.Match(tempTandA, pattern).Groups["title"].ToString();
-                        item.Author = Regex.Match(tempTandA, pattern).Groups["author"].ToString();
+                        item.Title = Regex.Match(tempTandA, pattern).Groups["title"].ToString().TrimEnd(new char[] { ' ' });
+                        item.Author = Regex.Match(tempTandA, pattern).Groups["author"].ToString().TrimEnd(new char[] { ' ' });
                         string tempHorB = reader.ReadLine(); // highlight or bookmark
                         reader.ReadLine();
-                        item.HighlightedText = reader.ReadLine();
+                        item.HighlightedText = reader.ReadLine().TrimEnd(new char[] { ' ' });
                         reader.ReadLine();
                         if (tempHorB.Contains("Highlight"))
                             clippingsList.Add(item);
